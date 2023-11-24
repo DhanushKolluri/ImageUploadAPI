1a45e73be5ae46008912a9c2a86a987fe515d77a
Dhanush Kolluri <kolluridhanush123@gmail.com>
2023-11-24 15:40:49 +0530

    UnSupported exception

---
+++ ImageUploadAPI/ImageUploadAPI/Controllers/ImageUploadController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        public ImageUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public class FileUploadAPI
+        {
+            public IFormFile files
+            { 
+                get;
+                set;
+            }
+        }
+        [HttpPost]
+        public async Task<string> Post([FromBody] FileUploadAPI fileUploadAPI)
+        {
+            if (fileUploadAPI.files.Length > 0)
+            {
+                try
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\uploads\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\uploads\\");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\uploads\\"+ fileUploadAPI.files.FileName))
+                    {
+                        fileUploadAPI.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "\\uploads\\" + fileUploadAPI.files.FileName;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+            else
+            {
+                return "Unsuccessful";
+            }
+        }
+    }
+}
--- ImageUploadAPI/ImageUploadAPI/Startup.cs
+++ ImageUploadAPI/ImageUploadAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -48,8 +49,10 @@ namespace ImageUploadAPI
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseStaticFiles();
 
+            app.UseAuthorization();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
